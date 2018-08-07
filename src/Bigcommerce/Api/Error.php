096199f83ee8742e6dd1b768a53f4001e8b83909
Sergey Golubev <snowman@cs-cart.com>
2018-08-07 11:45:07 +0400

    Update Error.php

--- src/Bigcommerce/Api/Error.php
+++ src/Bigcommerce/Api/Error.php
@@ -9,8 +9,16 @@ class Error extends \Exception
 {
     public function __construct($message, $code)
     {
-        if (is_array($message)) {
-            $message = $message[0]->message;
+        if (!is_string($message)) {
+            if (is_array($message) && !empty($message[0]->message)) {
+                $message = $message[0]->message;
+
+            } elseif (is_object($message) && !empty($message->title)) {
+                $message = $message->title;
+
+            } else {
+                $message = 'Unknown error';
+            }
         }
 
         parent::__construct($message, $code);
